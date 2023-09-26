88074d0791cb7ef38059f73e1cff095cc743b134
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-26 09:46:02 -0600

    cleanup drawing controls

--- src/components/SketchPad.utils.ts
+++ src/components/SketchPad.utils.ts
@@ -5,9 +5,8 @@ export const getPosition = (
   const rect = canvasRef?.getBoundingClientRect();
   if (evt.type === "mousedown" || evt.type === "mousemove") {
     const { clientX, clientY } = evt as React.MouseEvent<HTMLCanvasElement, MouseEvent>;
-    const x = Math.round(Math.abs(rect.x - clientX));
-    const y = Math.round(Math.abs(rect.y - clientY));
-    console.log({ x, y });
+    const x = Math.round(clientX - rect.left);
+    const y = Math.round(clientY - rect.top);
     return [x, y];
   } else if (evt instanceof TouchEvent) {
     evt.preventDefault();

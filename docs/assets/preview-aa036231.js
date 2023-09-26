e37281bb2263dd854a7114b6416d4f0526cc3808
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-25 18:57:23 -0600

    removed docs folder

--- docs/assets/preview-aa036231.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-6eb0f257.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-3fc0420c.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-3fc0420c.js","./iframe-6eb0f257.js","./index-c013ead5.js","./_commonjsHelpers-725317a4.js","./_getPrototype-dd8f0158.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-aa036231.js.map

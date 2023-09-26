79c5e7a8f52c3997eaa4c0d2466240009d3dece2
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-25 18:25:43 -0600

    created sketchpad template

---
+++ src/components/SketchPad.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SketchPad } from ".";
+
+const meta = {
+  title: "SketchPad",
+  component: SketchPad,
+} satisfies Meta<typeof SketchPad>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const WhiteSketchPad: Story = {
+  args: {
+    scale: [1, 1],
+    showExportJson: true,
+    showExportToPng: true,
+    showUndo: true,
+    size: 400,
+    styles: {
+      backgroundColor: "white",
+    },
+  },
+};
